dc0b766d586e32cf75535c21343c29991f860aa6
Ashutosh9795 <ashutoshg067@gmail.com>
2024-11-17 09:26:38 +0530

    upload

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -1,21 +1,13 @@
 import multer from 'multer';
 
-// Configure multer to use memory storage
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
 
-// Create the upload middleware
-export const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 10 * 1024 * 1024, // Limit file size to 10MB (optional)
-    },
-    fileFilter: (req, file, cb) => {
-        // Optional: Validate file types (e.g., only images)
-        const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
-        if (allowedMimeTypes.includes(file.mimetype)) {
-            cb(null, true); // Accept file
-        } else {
-            cb(new Error('Invalid file type. Only JPEG, PNG, and GIF are allowed.'), false); // Reject file
-        }
-    }
-});
+    destination: function (req, file, cb) {
+        cb(null, "./Public/temp")
+      },
+      filename: function (req, file, cb) {
+        cb(null, file.originalname)
+      }
+})
+
+export  const upload = multer({ storage: storage })
