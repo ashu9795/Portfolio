110326953dda908f5bb15ad72759fcd5571ec5fe
Ashutosh9795 <ashutoshg067@gmail.com>
2024-11-17 13:51:31 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://portfolio-5eri.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Removes '/api' prefix when forwarding requests.
       }
