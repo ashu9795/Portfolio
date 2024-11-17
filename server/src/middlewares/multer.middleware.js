efc31828c5648ba852ace5ba4e3b6cb6f097b54d
Ashutosh9795 <ashutoshg067@gmail.com>
2024-11-17 09:13:02 +0530

    changer mul;ter

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -1,13 +1,20 @@
 import multer from 'multer';
 
+import fs from 'fs/promises';
+
 const storage = multer.diskStorage({
+    destination: async function (req, file, cb) {
+        const tempDir = "./Public/temp";
+        try {
+            await fs.access(tempDir).catch(() => fs.mkdir(tempDir, { recursive: true }));
+            cb(null, tempDir);
+        } catch (err) {
+            cb(new Error("Failed to create temp directory"));
+        }
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
 
-    destination: function (req, file, cb) {
-        cb(null, "./Public/temp")
-      },
-      filename: function (req, file, cb) {
-        cb(null, file.originalname)
-      }
-})
-
-export  const upload = multer({ storage: storage })
+export const upload = multer({ storage: storage });
